cbc6c2facf531bd2c8a95af4fdd70f2595048378
Aritz Beobide-Cardinal <aritz@aritzcracker.ca>
2018-11-13 16:36:04 -0500

    It actually works now

--- node.js
+++ node.js
@@ -1,8 +1,8 @@
-const instantiatePbkdf2Lib = require("./pbkdf2.js");
+const {instantiatePbkdf2Lib} = require("./pbkdf2.js");
 const instantiatePbkdf2 = async (sha512, bytes) => {
 	if (bytes == null){
         return new Promise((resolve,reject) =>{
-            require("fs").readFile("./bin/pbkdf2.wasm",(err,data) => {
+            require("fs").readFile(__dirname+"/bin/pbkdf2.wasm",(err,data) => {
                 if (err){
                     reject(err);
                 }else{
